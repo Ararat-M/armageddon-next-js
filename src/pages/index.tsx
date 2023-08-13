0a3a2f6ab0302be8a367fa35c398ecaced1e0c67
Ararat <melkumyan2077@gmail.com>
2023-08-14 01:21:36 +0300

    add infinity scroll

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,61 @@
-import { useEffect, useState } from "react";
 import { type AsteroidSchema, asteroidService } from "@/entities/Asteroid";
 import { AsteroidList } from "@/widgets/AsteroidList";
-import { formatDateForApi } from "@/shared/lib/formatDateForApi/formatDateForApi";
+import Image from "next/image";
+import { useRef, useEffect } from "react";
 
 interface IndexProps {
   asteroids: AsteroidSchema[];
 }
 
 export default function Index({ asteroids }: IndexProps) {
+  const observerTarget = useRef(null);
+  const earthImage = useRef(null);
+  const observer = useRef<IntersectionObserver>(null);
+
+  useEffect(() => {
+    if (observer.current) observer.current.disconnect();
+
+    function callback (entries: IntersectionObserverEntry[], observer) {
+      if (!entries[0].isIntersecting) {
+        earthImage.current.style.top = "83px";
+      }
+
+      if (entries[0].isIntersecting) {
+        earthImage.current.style.top = "138px";
+      }
+
+      console.log("observer");
+    }
+
+    observer.current = new IntersectionObserver(callback);
+    observer.current.observe(observerTarget.current);
+  }, []);
+
   return (
     <div className="app">
-      <AsteroidList asteroids={asteroids}></AsteroidList>
+      <div ref={observerTarget} style={{ height: "123px" }}>
+        <h1>
+          ARMAGEDDON 2023
+        </h1>
+        <p>
+          ООО “Команда им. Б. Уиллиса”.<br />Взрываем астероиды с 1998 года.
+        </p>
+      </div>
+      <Image
+        ref={earthImage}
+        className="my-img"
+        priority
+        src="/planeta_zemlia_2560x1600.jpg"
+        width={377}
+        height={436}
+        alt="earth"
+      />
+      <div className="asteroid-list">
+        <AsteroidList asteroids={asteroids}></AsteroidList>
+      </div>
+      <div style={{ position: "fixed", top: "134px", right: "-111px" }}>
+        corzina
+      </div>
     </div>
   );
 }
--- src/widgets/AsteroidList/ui/AsteroidList.tsx
+++ src/widgets/AsteroidList/ui/AsteroidList.tsx
@@ -1,10 +1,11 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { asteroidService, type AsteroidSchema } from "@/entities/Asteroid";
 import { AsteroidItem } from "@/widgets/AsteroidItem";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import classes from "./asteroidList.module.scss";
 import { Button, ButtonTheme } from "@/shared/ui/Button/Button";
 import { useFetching } from "@/shared/hooks/useFetching";
+import { Loader } from "@/shared/ui/Loader/Loader";
 
 interface AsteroidListProps {
   asteroids: AsteroidSchema[];
@@ -15,6 +16,8 @@ export function AsteroidList({ asteroids }: AsteroidListProps) {
   const [sortedAsteroids, setSortedAsteroids] = useState<AsteroidSchema[]>([]);
   const [isDistanceInKm, setIsDistanceInKm] = useState(true);
   const [date, setDate] = useState(new Date());
+  const observerTarget = useRef<HTMLDivElement | null>(null);
+  const observer = useRef<IntersectionObserver>(null);
 
   const [isLoading, Error, fetchAsteroids] = useFetching(async () => {
     const newDate = new Date(+date + 86400 * 1000);
@@ -41,11 +44,26 @@ export function AsteroidList({ asteroids }: AsteroidListProps) {
     }));
   }, [asteroidsArr]);
 
+  useEffect(() => {
+    if (isLoading) return;
+    if (observer.current) observer.current.disconnect();
+
+    function callback (entries: IntersectionObserverEntry[], observer) {
+      if (entries[0].isIntersecting) {
+        fetchAsteroids();
+        console.log(document.body.scrollHeight);
+      }
+    }
+
+    observer.current = new IntersectionObserver(callback);
+    observer.current.observe(observerTarget.current);
+  }, [date, fetchAsteroids]);
+
   return (
     <div className={classes.content}>
       <div className={classes.head}>
         <h1 className={classes.title}>
-          Ближайшие подлёты астероидов
+          Ближайшие подлёты<br /> астероидов
         </h1>
         <div>
           <Button
@@ -74,6 +92,10 @@ export function AsteroidList({ asteroids }: AsteroidListProps) {
           </div>);
         })}
       </ul>
+      {isLoading
+        ? <div style={{ height: "80px" }}><Loader /></div>
+        : <div style={{ height: "80px" }}></div>}
+      <div ref={observerTarget}></div>
     </div>
   );
 }
